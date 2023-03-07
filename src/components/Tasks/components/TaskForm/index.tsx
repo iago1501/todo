9d742aea9ac98cc04c752aa36f0f84d4035ea840
Iago <iago1501@gmail.com>
2023-03-07 11:47:47 -0300

    :clown_face: mocking api

--- src/components/Tasks/components/TaskForm/index.tsx
+++ src/components/Tasks/components/TaskForm/index.tsx
@@ -1,44 +1,52 @@
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as zod from 'zod';
-import { PlusCircle } from 'phosphor-react';
-import { TaskForm } from './styles';
-import { useTasks } from '../../../../contexts/TasksContext';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { PlusCircle } from "phosphor-react";
+import { TaskForm } from "./styles";
+import { useTasks } from "../../../../contexts/TasksContext";
 
 const taskSchema = zod.object({
-  task: zod.string().min(3, { message: 'Conteúdo mínimo de 3 caracteres para uma tarefa' }),
+  task: zod
+    .string()
+    .min(3, { message: "Conteúdo mínimo de 3 caracteres para uma tarefa" }),
 });
 
-type taskSchemaData = zod.infer<typeof taskSchema>
+type taskSchemaData = zod.infer<typeof taskSchema>;
 
 export function Searchbar() {
+  const { createNewTask } = useTasks();
 
-  const {createNewTask} = useTasks()
-
-  const {register, handleSubmit, reset, formState: {errors, isSubmitting}} = useForm<taskSchemaData>({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<taskSchemaData>({
     resolver: zodResolver(taskSchema),
     defaultValues: {
-      task: ''
-    }
-  })
+      task: "",
+    },
+  });
 
   function handleCreateNewTask(data: taskSchemaData) {
-    createNewTask(data.task)
-    reset()
+    createNewTask(data.task);
+    reset();
   }
 
   return (
-    <TaskForm action="" onSubmit={handleSubmit(handleCreateNewTask)}>
-      <input type="text" 
-      required
-      placeholder='Adicione uma nova tarefa'
-      autoComplete='off'
-      {...register('task')}
-    />
-      <button type='submit'>
+    <TaskForm onSubmit={handleSubmit(handleCreateNewTask)}>
+      <input
+        type="text"
+        required
+        placeholder="Adicione uma nova tarefa"
+        title="Adicione uma nova tarefa"
+        autoComplete="off"
+        {...register("task")}
+      />
+      <button type="submit">
         Criar
-        <PlusCircle size={16}/>
+        <PlusCircle size={16} />
       </button>
     </TaskForm>
-  )
+  );
 }
